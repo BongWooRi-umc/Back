5ca9e4f2278a5b6167cb7a6a2f5dc9f6bf4de9ac
TroyKang <kdu3840@gmail.com>
2024-02-12 07:39:26 +0900

    FEAT community function

--- src/routes/community.post.article.route.js
+++ src/routes/community.post.article.route.js
@@ -1,5 +1,24 @@
 const express = require('express');
+const sequelize = require('sequelize');
 
 const articleRouter = express.Router();
+const {Article} = require('../models');
+
+articleRouter.post('/',async(req,res)=> {
+    const UserId = req.user ? req.user.id : null;
+
+    if (UserId) {
+    await Article.create({
+        title: title,
+        content: content,
+        userId: UserId
+      });
+    } else {
+        return res.status(400).json({
+            code: 400,
+            message: 'Unauthorized access.'
+          });
+        };
+})
 
 module.exports = {articleRouter};
